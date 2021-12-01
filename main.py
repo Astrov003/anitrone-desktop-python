b35752dd0ea9205ec648f5985f314757402aed35
Astrov003 <vpopovic003@gmail.com>
2021-12-01 17:34:13 +0100

    Attempt with OOP - I have cold, and I am frustrated

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtCore
+class Image_object():
+    def image():
+        image = QPixmap("./images/dot.png")
+        image = QPixmap.scaled(120, 120, QtCore.Qt.KeepAspectRatio)
+        img = QLabel()
+        img.setPixmap(image)
+        grid.addWidget(img, 0, 0)
+        
 
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(200, 200, 820, 120) #xposition, yposition, width, height
-        self.setWindowTitle("Anitrone")
-        self.initUI()
 
-    def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText("my first label")
-        self.label.move(50,50)
 
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText("Click me")
-        self.b1.clicked.connect(self.clicked)
+app = QApplication(sys.argv)
+window = QWidget()
+window.setWindowTitle("Anitrone")
+window.setGeometry(200, 200, 820, 120)
+window.setStyleSheet("background: black;")
 
-    def clicked(self):
-        self.label.setText("you pressed the button")
-        self.update()
+grid = QGridLayout()
 
-    def update(self):
-        self.label.adjustSize()
+window.setLayout(grid)
 
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow() 
-    win.show()
-    sys.exit(app.exec_())
+Image_object.image()
 
-window()
+window.show()
+sys.exit(app.exec())
