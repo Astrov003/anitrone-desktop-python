59110e795a147952147705b58ed0f1424777ddf3
Astrov003 <vpopovic003@gmail.com>
2021-12-01 18:33:43 +0100

    trying to implement click-on-image functionality

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtCore
+
+
 class Image_object():
-    def image():
+    def image(column):
         image = QPixmap("./images/dot.png")
-        image = QPixmap.scaled(120, 120, QtCore.Qt.KeepAspectRatio)
+        image = image.scaledToWidth(120)
+        image = image.scaledToHeight(120)
         img = QLabel()
         img.setPixmap(image)
-        grid.addWidget(img, 0, 0)
-        
+        grid.addWidget(img, 0, column)
+        img.mousePressEvent(click())
+    
+    def click():
+        print("mrs u picku materinu")
 
 
 
@@ -24,7 +30,14 @@ grid = QGridLayout()
 
 window.setLayout(grid)
 
-Image_object.image()
+Image_object.image(0)
+Image_object.image(1)
+Image_object.image(2)
+Image_object.image(3)
+Image_object.image(4)
+Image_object.image(5)
+Image_object.image(6)
+Image_object.image(7)
 
 window.show()
 sys.exit(app.exec())
