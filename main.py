fb76183c36ad32d7b9f2db0e5653e38cf2e6df45
Astrov003 <vpopovic003@gmail.com>
2021-12-03 20:58:22 +0100

    done: 1. code formatted, 2. full switch image for all 7 buttons; todo: 1. glow animation, 2. render

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QGridLayout, QWidget, QLabel, QGraphicsOpacityEffect, QShortcut
 from PyQt5.QtGui import QPixmap, QKeySequence
-from PyQt5 import QtCore, QtTest
-import time
-#from PyQt5.QtCore import Qt, pyqtSignal, QRect
-#from PyQt5.QtCore import QPropertyAnimation
+from PyQt5 import QtCore
 
 
+#====== MAIN WINDOW CLASS =================
 class MyWindow(QWidget):
     def __init__(self):
         super(MyWindow, self).__init__()
@@ -16,48 +14,168 @@ class MyWindow(QWidget):
         self.setLayout(grid)
         self.initUI()
 
+    # === Keyboard Shortcut Event ======
     def initUI(self):
         self.msgSc = QShortcut(QKeySequence('Ctrl+1'), self)
         self.msgSc.activated.connect(lambda *_: trigger_glow())
     
 
-# INIT BUTTONS + CLICK DEFINED
+#====== IMAGE CLASS + IMAGE CLICK EVENT ======
 
 class Image0(QLabel):
     def mousePressEvent(self, event):
-        print ("clicked0")
-        btn0.setPixmap(image1)
-
+        global btn0_image_counter
+        if btn0_image_counter == 0:
+            btn0.setPixmap(arr_images_base[1])
+            btn0_image_counter = 1
+        elif btn0_image_counter == 1:
+            btn0.setPixmap(arr_images_base[2])
+            btn0_image_counter = 2
+        elif btn0_image_counter == 2:
+            btn0.setPixmap(arr_images_base[3])
+            btn0_image_counter = 3
+        elif btn0_image_counter == 3:
+            btn0.setPixmap(arr_images_base[4])
+            btn0_image_counter = 4
+        elif btn0_image_counter == 4:
+            btn0.setPixmap(arr_images_base[0])
+            btn0_image_counter = 0
+        
 class Image1(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked1")
+        global btn1_image_counter
+        if btn1_image_counter == 0:
+            btn1.setPixmap(arr_images_base[1])
+            btn1_image_counter = 1
+        elif btn1_image_counter == 1:
+            btn1.setPixmap(arr_images_base[2])
+            btn1_image_counter = 2
+        elif btn1_image_counter == 2:
+            btn1.setPixmap(arr_images_base[3])
+            btn1_image_counter = 3
+        elif btn1_image_counter == 3:
+            btn1.setPixmap(arr_images_base[4])
+            btn1_image_counter = 4
+        elif btn1_image_counter == 4:
+            btn1.setPixmap(arr_images_base[0])
+            btn1_image_counter = 0
 
 class Image2(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked2")
+        global btn2_image_counter
+        if btn2_image_counter == 0:
+            btn2.setPixmap(arr_images_base[1])
+            btn2_image_counter = 1
+        elif btn2_image_counter == 1:
+            btn2.setPixmap(arr_images_base[2])
+            btn2_image_counter = 2
+        elif btn2_image_counter == 2:
+            btn2.setPixmap(arr_images_base[3])
+            btn2_image_counter = 3
+        elif btn2_image_counter == 3:
+            btn2.setPixmap(arr_images_base[4])
+            btn2_image_counter = 4
+        elif btn2_image_counter == 4:
+            btn2.setPixmap(arr_images_base[0])
+            btn2_image_counter = 0
 
 class Image3(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked3")
+        global btn3_image_counter
+        if btn3_image_counter == 0:
+            btn3.setPixmap(arr_images_base[1])
+            btn3_image_counter = 1
+        elif btn3_image_counter == 1:
+            btn3.setPixmap(arr_images_base[2])
+            btn3_image_counter = 2
+        elif btn3_image_counter == 2:
+            btn3.setPixmap(arr_images_base[3])
+            btn3_image_counter = 3
+        elif btn3_image_counter == 3:
+            btn3.setPixmap(arr_images_base[4])
+            btn3_image_counter = 4
+        elif btn3_image_counter == 4:
+            btn3.setPixmap(arr_images_base[0])
+            btn3_image_counter = 0
 
 class Image4(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked4")
+        global btn4_image_counter
+        if btn4_image_counter == 0:
+            btn4.setPixmap(arr_images_base[1])
+            btn4_image_counter = 1
+        elif btn4_image_counter == 1:
+            btn4.setPixmap(arr_images_base[2])
+            btn4_image_counter = 2
+        elif btn4_image_counter == 2:
+            btn4.setPixmap(arr_images_base[3])
+            btn4_image_counter = 3
+        elif btn4_image_counter == 3:
+            btn4.setPixmap(arr_images_base[4])
+            btn4_image_counter = 4
+        elif btn4_image_counter == 4:
+            btn4.setPixmap(arr_images_base[0])
+            btn4_image_counter = 0
 
 class Image5(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked5")
+        global btn5_image_counter
+        if btn5_image_counter == 0:
+            btn5.setPixmap(arr_images_base[1])
+            btn5_image_counter = 1
+        elif btn5_image_counter == 1:
+            btn5.setPixmap(arr_images_base[2])
+            btn5_image_counter = 2
+        elif btn5_image_counter == 2:
+            btn5.setPixmap(arr_images_base[3])
+            btn5_image_counter = 3
+        elif btn5_image_counter == 3:
+            btn5.setPixmap(arr_images_base[4])
+            btn5_image_counter = 4
+        elif btn5_image_counter == 4:
+            btn5.setPixmap(arr_images_base[0])
+            btn5_image_counter = 0
 
 class Image6(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked6")
+        global btn6_image_counter
+        if btn6_image_counter == 0:
+            btn6.setPixmap(arr_images_base[1])
+            btn6_image_counter = 1
+        elif btn6_image_counter == 1:
+            btn6.setPixmap(arr_images_base[2])
+            btn6_image_counter = 2
+        elif btn6_image_counter == 2:
+            btn6.setPixmap(arr_images_base[3])
+            btn6_image_counter = 3
+        elif btn6_image_counter == 3:
+            btn6.setPixmap(arr_images_base[4])
+            btn6_image_counter = 4
+        elif btn6_image_counter == 4:
+            btn6.setPixmap(arr_images_base[0])
+            btn6_image_counter = 0
 
 class Image7(QLabel):     
     def mousePressEvent(self, event):
-        print ("clicked7")
+        global btn7_image_counter
+        if btn7_image_counter == 0:
+            btn7.setPixmap(arr_images_base[1])
+            btn7_image_counter = 1
+        elif btn7_image_counter == 1:
+            btn7.setPixmap(arr_images_base[2])
+            btn7_image_counter = 2
+        elif btn7_image_counter == 2:
+            btn7.setPixmap(arr_images_base[3])
+            btn7_image_counter = 3
+        elif btn7_image_counter == 3:
+            btn7.setPixmap(arr_images_base[4])
+            btn7_image_counter = 4
+        elif btn7_image_counter == 4:
+            btn7.setPixmap(arr_images_base[0])
+            btn7_image_counter = 0
 
 
-# FADE IN IMAGES
+#====== FADE IN LOGIC =====================
 
 class FadeInImage0(QLabel):
     def __init__(self):
@@ -83,28 +201,14 @@ class FadeInImage0(QLabel):
 
 
 def trigger_glow():
-    global btn0_image
-    if btn0_image == 0:
-        image0_glow = QPixmap('./images/dot_glow.png')
-        image0_glow = image0_glow.scaledToWidth(120)
-        image0_glow = image0_glow.scaledToHeight(120)
-        img0_glow = FadeInImage0()
-        img0_glow.setPixmap(image0_glow)
-        grid.addWidget(img0_glow, 0, 0)
+    global btn0_image_counter
+    if btn0_image_counter == 0:     
+        btn0_glow = FadeInImage0()
+        btn0_glow.setPixmap(arr_images_glow[0])
+        grid.addWidget(btn0_glow, 0, 0)
         
         
-    
 
-#==== GLOBAL VARIABLES====
-
-btn0_image = 0
-btn1_image = 0
-btn2_image = 0
-btn3_image = 0
-btn4_image = 0
-btn5_image = 0
-btn6_image = 0
-btn7_image = 0
 
 #==== MAIN ================
 
@@ -113,91 +217,70 @@ grid = QGridLayout()
 
 win = MyWindow()
 
-# BUTTONS
-
-button0 = QPixmap('./images/dot.png')
-button0 = button0.scaledToWidth(120)
-button0 = button0.scaledToHeight(120)
-
-btn0 = Image0()
-btn0.setPixmap(button0)
-
-
-btn1 = Image1()
-btn1.setPixmap(button0)
-btn2 = Image2()
-btn2.setPixmap(button0)
-btn3 = Image3()
-btn3.setPixmap(button0)
-btn4 = Image4()
-btn4.setPixmap(button0)
-btn5 = Image5()
-btn5.setPixmap(button0)
-btn6 = Image6()
-btn6.setPixmap(button0)
-btn7 = Image7()
-btn7.setPixmap(button0)
-
-
-### BASE IMAGES
+###====== IMAGE VARIABLES ================================================
+btn0_image_counter = 0
+btn1_image_counter = 0
+btn2_image_counter = 0
+btn3_image_counter = 0
+btn4_image_counter = 0
+btn5_image_counter = 0
+btn6_image_counter = 0
+btn7_image_counter = 0
 
 image0 = QPixmap('./images/dot.png')
 image0 = image0.scaledToWidth(120)
 image0 = image0.scaledToHeight(120)
-img0 = QLabel()
-img0.setPixmap(image0)
 image1 = QPixmap('./images/down_full.png')
 image1 = image1.scaledToWidth(120)
 image1 = image1.scaledToHeight(120)
-img1 = QLabel()
-img1.setPixmap(image1)
 image2 = QPixmap('./images/down_open.png')
 image2 = image2.scaledToWidth(120)
 image2 = image2.scaledToHeight(120)
-img2 = QLabel()
-img2.setPixmap(image2)
 image3 = QPixmap('./images/up_full.png')
 image3 = image3.scaledToWidth(120)
 image3 = image3.scaledToHeight(120)
-img3 = QLabel()
-img3.setPixmap(image3)
 image4 = QPixmap('./images/up_open.png')
 image4 = image4.scaledToWidth(120)
 image4 = image4.scaledToHeight(120)
-img4 = QLabel()
-img4.setPixmap(image4)
 
-
-images = [img0, img1, img2, img3, img4]
-
-### GLOW IMAGES
-
-
-
-
+image0_glow = QPixmap('./images/dot_glow.png')
+image0_glow = image0_glow.scaledToWidth(120)
+image0_glow = image0_glow.scaledToHeight(120)
 image1_glow = QPixmap('./images/down_full_glow.png')
 image1_glow = image1_glow .scaledToWidth(120)
 image1_glow = image1_glow .scaledToHeight(120)
-img1_glow= QLabel()
-img1_glow.setPixmap(image1_glow)
 image2_glow = QPixmap('./images/down_open_glow.png')
 image2_glow = image2_glow .scaledToWidth(120)
 image2_glow = image2_glow .scaledToHeight(120)
-img2_glow = QLabel()
-img2_glow.setPixmap(image2_glow)
 image3_glow = QPixmap('./images/up_full_glow.png')
 image3_glow = image3_glow .scaledToWidth(120)
 image3_glow = image3_glow .scaledToHeight(120)
-img3_glow = QLabel()
-img3_glow.setPixmap(image3_glow)
 image4_glow = QPixmap('./images/up_open_glow.png')
 image4_glow = image4_glow .scaledToWidth(120)
 image4_glow = image4_glow .scaledToHeight(120)
-img4_glow = QLabel()
-img4_glow.setPixmap(image4_glow)
+
+arr_images_base = [image0, image1, image2, image3, image4]
+arr_images_glow = [image0_glow, image1_glow, image2_glow, image3_glow, image4_glow]
+#======================================================================
 
 
-#images_glow = [img0_glow, img1_glow, img2_glow, img3_glow, img4_glow]
+#=== INIATIALIZE IMAGES =====
+btn0 = Image0()
+btn0.setPixmap(arr_images_base[0])
+btn1 = Image1()
+btn1.setPixmap(arr_images_base[0])
+btn2 = Image2()
+btn2.setPixmap(arr_images_base[0])
+btn3 = Image3()
+btn3.setPixmap(arr_images_base[0])
+btn4 = Image4()
+btn4.setPixmap(arr_images_base[0])
+btn5 = Image5()
+btn5.setPixmap(arr_images_base[0])
+btn6 = Image6()
+btn6.setPixmap(arr_images_base[0])
+btn7 = Image7()
+btn7.setPixmap(arr_images_base[0])
 
 grid.addWidget(btn0, 0, 0)
 grid.addWidget(btn1, 0, 1)
@@ -209,6 +292,8 @@ grid.addWidget(btn6, 0, 6)
 grid.addWidget(btn7, 0, 7)
 
 
+
+#=== SYSTEM COMMANDS =====
 win.show()
 
 sys.exit(app.exec_())
