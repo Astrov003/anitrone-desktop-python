01886dbe28d13aeb86118baa05ff7b721f4f45ff
Astrov003 <vpopovic003@gmail.com>
2021-12-07 15:59:36 +0100

    minor code format changes

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import os
 import sys
 from PyQt5.QtWidgets import QApplication, QGridLayout, QWidget, QLabel, QGraphicsOpacityEffect, QShortcut
 from PyQt5.QtGui import QPixmap, QKeySequence
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 
+#=== ANIMATION PARAMETERS ====
 
 ATTACK = 100
-HOLD = 0
-RELEASE = 100
+HOLD = ATTACK + 0 # delays release from the beggining of attack, hence attack + additional time 
+RELEASE = 1000
 
 
-#=== ANIMATION PLAY ======
+#=== ANIMATION PLAY ==========
 
 def play(TEMPO):
     if TEMPO == 120:
@@ -102,12 +104,12 @@ class FadeInImage0(QLabel):
     def __init__(self):
         super().__init__()
         self.fadeIn()
-        QtCore.QTimer.singleShot(1000, lambda:self.fadeOut())
+        QtCore.QTimer.singleShot(HOLD, lambda:self.fadeOut())
     def fadeIn(self):   
         self.effect = QGraphicsOpacityEffect()
         self.setGraphicsEffect(self.effect)
         self.animation = QtCore.QPropertyAnimation(self.effect, b"opacity")
-        self.animation.setDuration(1000)
+        self.animation.setDuration(ATTACK)
         self.animation.setStartValue(0)
         self.animation.setEndValue(1)
         self.animation.start()
@@ -115,7 +117,7 @@ class FadeInImage0(QLabel):
         self.effect = QGraphicsOpacityEffect()
         self.setGraphicsEffect(self.effect)
         self.animation = QtCore.QPropertyAnimation(self.effect, b"opacity")
-        self.animation.setDuration(1000)
+        self.animation.setDuration(RELEASE)
         self.animation.setStartValue(1)
         self.animation.setEndValue(0)
         self.animation.start()
@@ -278,6 +280,7 @@ class Image7(QLabel):
 
     
 #====== MAIN WINDOW CLASS =================
+
 class MyWindow(QWidget):
     def __init__(self):
         super(MyWindow, self).__init__()
@@ -297,6 +300,18 @@ class MyWindow(QWidget):
         self.tempo180.activated.connect(lambda *_: play(180))
 
 
+#======= FIX WHEN COMPILING ONE FILE ======
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 #==== MAIN ================
 
 app = QApplication(sys.argv)
@@ -314,35 +329,35 @@ btn5_image_counter = 0
 btn6_image_counter = 0
 btn7_image_counter = 0
 
-image0 = QPixmap('./images/dot.png')
+image0 = QPixmap(resource_path('images/dot.png'))
 image0 = image0.scaledToWidth(120, Qt.SmoothTransformation)
 image0 = image0.scaledToHeight(120, Qt.SmoothTransformation)
-image1 = QPixmap('./images/down_full.png')
+image1 = QPixmap(resource_path('images/down_full.png'))
 image1 = image1.scaledToWidth(120, Qt.SmoothTransformation)
 image1 = image1.scaledToHeight(120, Qt.SmoothTransformation)
-image2 = QPixmap('./images/down_open.png')
+image2 = QPixmap(resource_path('images/down_open.png'))
 image2 = image2.scaledToWidth(120, Qt.SmoothTransformation)
 image2 = image2.scaledToHeight(120, Qt.SmoothTransformation)
-image3 = QPixmap('./images/up_full.png')
+image3 = QPixmap(resource_path('images/up_full.png'))
 image3 = image3.scaledToWidth(120, Qt.SmoothTransformation)
 image3 = image3.scaledToHeight(120, Qt.SmoothTransformation)
-image4 = QPixmap('./images/up_open.png')
+image4 = QPixmap(resource_path('images/up_open.png'))
 image4 = image4.scaledToWidth(120, Qt.SmoothTransformation)
 image4 = image4.scaledToHeight(120, Qt.SmoothTransformation)
 
-image0_glow = QPixmap('./images/dot_glow.png')
+image0_glow = QPixmap(resource_path('images/dot_glow.png'))
 image0_glow = image0_glow.scaledToWidth(120, Qt.SmoothTransformation)
 image0_glow = image0_glow.scaledToHeight(120, Qt.SmoothTransformation)
-image1_glow = QPixmap('./images/down_full_glow.png')
+image1_glow = QPixmap(resource_path('images/down_full_glow.png'))
 image1_glow = image1_glow .scaledToWidth(120, Qt.SmoothTransformation)
 image1_glow = image1_glow .scaledToHeight(120, Qt.SmoothTransformation)
-image2_glow = QPixmap('./images/down_open_glow.png')
+image2_glow = QPixmap(resource_path('images/down_open_glow.png'))
 image2_glow = image2_glow .scaledToWidth(120, Qt.SmoothTransformation)
 image2_glow = image2_glow .scaledToHeight(120, Qt.SmoothTransformation)
-image3_glow = QPixmap('./images/up_full_glow.png')
+image3_glow = QPixmap(resource_path('images/up_full_glow.png'))
 image3_glow = image3_glow .scaledToWidth(120, Qt.SmoothTransformation)
 image3_glow = image3_glow .scaledToHeight(120, Qt.SmoothTransformation)
-image4_glow = QPixmap('./images/up_open_glow.png')
+image4_glow = QPixmap(resource_path('images/up_open_glow.png'))
 image4_glow = image4_glow .scaledToWidth(120, Qt.SmoothTransformation)
 image4_glow = image4_glow .scaledToHeight(120, Qt.SmoothTransformation)
 
--- render.py
+++ render.py
@@ -3,14 +3,17 @@ import numpy as np
 import cv2
 import ctypes
 from ctypes.wintypes import HWND, DWORD, RECT
+import pywintypes
+from win32 import win32gui
 import datetime
 
 dwmapi = ctypes.WinDLL("dwmapi")
-hwnd = 787684    # refer to the other answers on how to find the hwnd of your window
+
+hwnd = win32gui.FindWindow(None, 'anitrone')
+
 rect = RECT()
 DMWA_EXTENDED_FRAME_BOUNDS = 9
-dwmapi.DwmGetWindowAttribute(HWND(hwnd), DWORD(DMWA_EXTENDED_FRAME_BOUNDS),
-                             ctypes.byref(rect), ctypes.sizeof(rect))
+dwmapi.DwmGetWindowAttribute(HWND(hwnd), DWORD(DMWA_EXTENDED_FRAME_BOUNDS), ctypes.byref(rect), ctypes.sizeof(rect))
 
 print(rect.left, rect.top, rect.right, rect.bottom)
 
@@ -19,11 +22,16 @@ file_name = f'{time_stamp}.mp4'
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 captured_video = cv2.VideoWriter(file_name, fourcc, 30.0, (rect.right-rect.left, rect.bottom-rect.top-50))
 
+width = 800
+height = 600
+
 while True:
-    img = ImageGrab.grab(bbox=(rect.left, rect.top+50, rect.right, rect.bottom))
+    img = ImageGrab.grab(bbox=(0, 0, width, height))
+    #img = ImageGrab.grab(bbox=(rect.left, rect.top+50, rect.right, rect.bottom))
     img_np = np.array(img)
     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
     cv2.imshow('Capturer', img_final)
+    
     captured_video.write(img_final)
     
     if cv2.waitKey(10) == ord('q'):
