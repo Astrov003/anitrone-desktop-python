9bb5c03075cfeb1f2913234aa4761f9182543c77
Astrov003 <vpopovic003@gmail.com>
2022-01-06 22:55:40 +0100

    for loop in use instead of while loop in render. gives 100% precision with total video time and frame count

--- render.py
+++ render.py
@@ -17,21 +17,14 @@ def record(duration):
     dwmapi.DwmGetWindowAttribute(HWND(hwnd), DWORD(DMWA_EXTENDED_FRAME_BOUNDS), ctypes.byref(rect), ctypes.sizeof(rect))
 
     #print(rect.left, rect.top, rect.right, rect.bottom)
-
+    FPS = 30
     time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
-    file_name = f'{time_stamp}.mp4'
+    file_name = f'{time_stamp}.avi'
     fourcc = cv2.VideoWriter_fourcc(*'FFV1')
-    captured_video = cv2.VideoWriter(file_name, fourcc, 30.0, (rect.right-rect.left, rect.bottom-rect.top-34))
+    captured_video = cv2.VideoWriter(file_name, fourcc, FPS, (rect.right-rect.left, rect.bottom-rect.top-34))
+    
 
-    start_time = time.time()
-
-    stime = str(time.time())
-    print("R: " + stime)
-    while True:
-        
-        
-        current_time = time.time()
-        elapsed_time = current_time - start_time
+    for i in range(int(duration * FPS)):
         
         img = ImageGrab.grab(bbox=(rect.left, rect.top+32, rect.right, rect.bottom-2))
         img_np = np.array(img)
@@ -39,5 +32,5 @@ def record(duration):
         #autocv2.imshow('Capturer', img_final)
         captured_video.write(img_final)
 
-        if elapsed_time > duration:
-            break
+    cv2.destroyAllWindows()
+    captured_video.release()  
